8aa2ee1526568fb3cae6fa738a2edf576086159c
Andrew Kreps <andrew.kreps@gmail.com>
2016-04-13 22:21:46 -0700

    Attachments v2

--- index.js
+++ index.js
@@ -5,13 +5,39 @@ app.get('/', function (req, res) {
     console.log(res);
     response = {
         "response_type": "in_channel",
-        "text": "Ya herd",
+        // "text": "Ya herd",
         "attachments": [
             {
-                "image_url": "https://media.giphy.com/media/r75ImMRYtSpYQ/giphy.gif"
+                "fallback": "HERD!",
+
+                "color": "#36a64f",
+
+                // "pretext": "Optional text that appears above the attachment block",
+
+                // "author_name": "Bobby Tables",
+                // "author_link": "http://flickr.com/bobby/",
+                // "author_icon": "http://flickr.com/icons/bobby.jpg",
+
+                "title": "Ya herd!",
+                "title_link": "https://media.giphy.com/media/r75ImMRYtSpYQ/giphy.gif",
+
+                // "text": "Optional text that appears within the attachment",
+
+                "fields": [
+                    {
+                        "title": "Priority",
+                        "value": "High",
+                        "short": false
+                    }
+                ],
+
+
+                "image_url": "https://media.giphy.com/media/r75ImMRYtSpYQ/giphy.gif",
+                // "thumb_url": "http://example.com/path/to/thumb.png"
             }
         ]
     };
+
     console.log(response);
     res.send(response);
 });
